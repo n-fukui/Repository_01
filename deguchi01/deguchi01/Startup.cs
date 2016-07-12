72964199ffc9bee2f9d389dbb3b6f93237a9b4a5
n-fukui <n_fukui@tokyojcs.com>
2016-07-12 14:25:54 +0900

    deguchi0101

---
+++ deguchi01/deguchi01/Startup.cs
@@ -0,0 +1,14 @@
+﻿using Microsoft.Owin;
+using Owin;
+
+[assembly: OwinStartupAttribute(typeof(deguchi01.Startup))]
+namespace deguchi01
+{
+    public partial class Startup
+    {
+        public void Configuration(IAppBuilder app)
+        {
+            ConfigureAuth(app);
+        }
+    }
+}
